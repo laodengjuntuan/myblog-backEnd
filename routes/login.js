1186ebb4f5a84a11f77fe9b4c06a42474e3cbcdb
xiaohua <3103506013@qq.com>
2021-12-17 16:11:19 +0800

    add README

--- routes/login.js
+++ routes/login.js
@@ -9,13 +9,14 @@ var connection = mysql.createConnection({
 })
 connection.connect()
 
+router.get('/', express.static('public/login'))
 router.post('/', function(req, res, next) {
-    let user = JSON.parse(req.body)
+    let user = req.body
     connection.query(`SELECT password FROM users WHERE name='${user.name}'`, function(err, result, field) {
         let password = result[0].password
         if (user.password == password) {
           req.session.username = user.name
-            res.send('http://localhost:2999/background')
+            res.send(user.name)
         } else {
             res.send('wrong')
         }
